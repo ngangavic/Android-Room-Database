d13720dfd0770598508ae56403f45aa6c8b06e42
ngangavic <ngangavictor10@gmail.com>
2019-12-06 11:39:21 +0300

    insert and display data

--- app/src/main/java/com/example/androidroomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/androidroomdatabase/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.androidroomdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread {
+            val db = Room.databaseBuilder(
+                applicationContext,
+                AppDatabase::class.java, "todo-list.db"
+            ).build()
+
+            var ent = TodoEntity(0, "Home", "Do web development")
+
+            db.todoDao().insertAll(ent)
+
+            db.todoDao().getAll().forEach {
+                Log.d("DATA:", it.toString())
+            }
+
+        }.start()
+
     }
 }
---
+++ app/src/main/java/com/example/androidroomdatabase/TodoEntity.kt
@@ -0,0 +1,16 @@
+package com.example.androidroomdatabase
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todo_items")
+data class TodoEntity (
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+
+    @ColumnInfo(name = "title") var title: String,
+    @ColumnInfo(name = "content") var content: String
+
+
+)

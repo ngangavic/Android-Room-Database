07de71a19b235f4347302e985b80e9d47f33b08e
ngangavic <ngangavictor10@gmail.com>
2020-05-01 13:30:09 +0300

    word dao

---
+++ app/src/main/java/com/example/androidroomdatabase/WordDao.kt
@@ -0,0 +1,18 @@
+package com.example.androidroomdatabase
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface WordDao {
+
+    @Insert
+    fun insert(word:Word)
+
+    @Query("DELETE FROM word_table")
+    fun deleteAll()
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAllWords():List<Word>
+}

03c4da37605ec6007f17fbd12e702465215c0e8b
ngangavic <ngangavictor10@gmail.com>
2020-05-01 13:29:52 +0300

    word entity

---
+++ app/src/main/java/com/example/androidroomdatabase/Word.kt
@@ -0,0 +1,19 @@
+package com.example.androidroomdatabase
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+class Word {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    var word:String
+
+    constructor(@NonNull word: String) {
+        this.word = word
+    }
+
+}

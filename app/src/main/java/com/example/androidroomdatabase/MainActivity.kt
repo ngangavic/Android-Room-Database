6f538a5a987da6d7e2249cd1b4615f58a062912a
ngangavic <ngangavictor10@gmail.com>
2020-05-03 11:39:52 +0300

    main activity class

--- app/src/main/java/com/example/androidroomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/androidroomdatabase/MainActivity.kt
@@ -1,46 +1,64 @@
 package com.example.androidroomdatabase
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textView:TextView
+
+    private lateinit var recyclerView: RecyclerView
+    lateinit var recyclerViewAdapter: WordListAdapter
+    private lateinit var wordViewModel: WordViewModel
+    private val newWordActivityRequestCode = 100
+    lateinit var floatingActionButton:FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.recyclerView)
+        floatingActionButton = findViewById(R.id.floatingActionButton)
+        recyclerViewAdapter = WordListAdapter(this)
+        recyclerView.adapter = recyclerViewAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        wordViewModel = ViewModelProvider(this).get(WordViewModel::class.java)
+        wordViewModel.allWords.observe(this, Observer { words ->
+            words?.let { recyclerViewAdapter.setWords(it) }
+        })
 
-        val editTextTitle=findViewById<EditText>(R.id.editTextTitle)
-        val editTextList=findViewById<EditText>(R.id.editTextList)
-        val buttonSave=findViewById<Button>(R.id.buttonSave)
-        val buttonView=findViewById<Button>(R.id.buttonView)
-        textView=findViewById<TextView>(R.id.textViewView)
-
-        buttonSave.setOnClickListener {
-
-        }
-        buttonView.setOnClickListener {
-            editTextTitle.visibility= View.GONE
-            editTextList.visibility= View.GONE
-            buttonSave.visibility= View.GONE
-            buttonView.visibility= View.GONE
-
+        floatingActionButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
         }
 
     }
 
-    fun display(text:java.lang.StringBuilder){
-        runOnUiThread {
-            // Stuff that updates the UI
-            textView.text=text
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra("EXTRA_REPLY")?.let {
+                val word = Word(it)
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Not saved",
+                Toast.LENGTH_LONG).show()
         }
     }
-
 }
